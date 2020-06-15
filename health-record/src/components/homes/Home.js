150ac7578b61277944986c70399e05d898c80b20
Kito Mam <kitomam@yahoo.com>
2020-06-15 14:48:08 -0700

    2nd test commit

---
+++ health-record/src/components/homes/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                <h style={{textAlign: "center"}}>Home</h>
+            </div>
+        )
+    }
+}
+
+export default Home;
