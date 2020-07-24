c3b7edbe973bedc095ee49b0de9cb53c2ae9c800
Evan Tung <dragonejt@gmail.com>
2020-07-24 14:17:40 -0700

    Added search bar for ptreat

--- health-record/src/components/treatment/Condition.js
+++ health-record/src/components/treatment/Condition.js
@@ -1,30 +1,27 @@
-import React, {Component} from 'react'
-import {Container, Col, Row, Button} from 'react-bootstrap'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { Container, Col, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 class Condition extends Component {
-
     componentDidMount() {
-        document.body.style.backgroundColor = "#19709c"
+        document.body.style.backgroundColor = "#19709c";
     }
 
     render() {
         return (
             <div className="col col-lg-4">
-              <p id="title"></p>
-              <hr>
-              <p id="start"></p>
-              <p id="end"></p>
-              <hr>
-              <p id="patient"</p>
-              <p id="doctor"></p>
-              <hr>
-              <p id="treatment"></p>
-              <hr>
-              <p id="notes"></p>
-
+                <p id="title"></p>
+                <hr />
+                <p id="start"></p>
+                <p id="end"></p>
+                <hr />
+                <p id="patient"></p>
+                <p id="doctor"></p>
+                <hr />
+                <p id="treatment"></p>
+                <hr />
+                <p id="notes"></p>
             </div>
-
-        )
+        );
     }
 }
 
--- health-record/src/components/treatment/Ptreat.js
+++ health-record/src/components/treatment/Ptreat.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react'
-import { Container, Col, Row, Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Container, Col, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 class Ptreat extends Component {
-
     componentDidMount() {
-        document.body.style.backgroundColor = "#19709c"
+        document.body.style.backgroundColor = "#19709c";
     }
 
     render() {
         return (
             <div className="container">
-              <br className="my-4">
-              <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-              <div class="input-group-append">
-              <span class="material-icons">
-                search
-              </span>
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">With textarea</span>
+                <br className="my-4" />
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">$</span>
+                    </div>
+                    <input
+                        type="text"
+                        class="form-control"
+                        aria-label="Amount (to the nearest dollar)"
+                    />
+                    <div class="input-group-append">
+                        <span class="input-group-text"></span>
+                    </div>
                 </div>
-              <textarea class="form-control" aria-label="With textarea"></textarea>
-              </div>
-      </div>
-
-      </div>
-
-        )
+            </div>
+        );
     }
 }
 
