d144269d6e6da113d579730f392ab69af056b3d0
Gayathri95 <s.gayu95@gmail.com>
2020-07-29 20:10:36 -0700

    change to accomodate port of node server

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
+app.set('port', process.env.PORT || 3000);
 app.listen(3000);
 console.log("app running");
