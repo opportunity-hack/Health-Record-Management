0e0ffc7b1e0f325a63cf9b61b43b23b17807f58e
Gayathri95 <s.gayu95@gmail.com>
2020-07-29 20:35:22 -0700

    setting port right

--- server.js
+++ server.js
@@ -6,6 +6,5 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
-app.set('port', process.env.PORT || 3000);
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("app running");
