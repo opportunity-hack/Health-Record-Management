4b80927e988708c71c58d9cc903881a3f7146ea5
Gayathri95 <s.gayu95@gmail.com>
2020-07-30 12:19:42 -0700

    Modifying for API calls for GET calls

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var routes = require("./routes/routes.js");
 var app = express();
 
 app.use(bodyParser.json());
+//app.use(express.json());
+
+//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+
 app.use('/', routes);
 app.listen(process.env.PORT || 3000);
 console.log("app running");
