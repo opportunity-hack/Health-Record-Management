31fa11f86eef217e94db48325f5b82f1bfd18a52
Gayathri95 <s.gayu95@gmail.com>
2020-08-01 19:38:58 -0700

    CORS

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(function(req, res, next) {
 
+	  res.header("Access-Control-Allow-Origin", "*");
+
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+	  next();
+
+});
 app.use('/', routes);
 app.listen(process.env.PORT || 3000);
 console.log("app running");
